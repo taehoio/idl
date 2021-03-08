9ae53efd2a1e2b820e55d37b56c8e9414e64032d
Taeho Kim <taeho@taeho.io>
2021-03-08 14:50:51 +0900

    Add grpc-gateway

--- scripts/generate.sh
+++ scripts/generate.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 IDL_PATH=/go/src/github.com/taehoio/idl
+PROTOBUF_COMMIT_SHA=e5baa4cc342f2e90f7ed531de4a59607e80d8e6d
 
-docker run --rm -i --name protobuf -v "$(pwd)":$IDL_PATH -w $IDL_PATH --entrypoint /bin/sh taehoio/protobuf:ff6526f768ccabbc821ac3fa52130dba40fcf513 -c '\
+docker run --rm -i --name protobuf -v "$(pwd)":$IDL_PATH -w $IDL_PATH --entrypoint /bin/sh taehoio/protobuf:$PROTOBUF_COMMIT_SHA -c '\
   buf generate \
 '
