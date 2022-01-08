ae51002587043c999f25640d1dbe54228d55a22b
Taeho Kim <taeho@taeho.io>
2022-01-09 02:31:43 +0900

    Fix package paths

--- scripts/generate.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-IDL_PATH=/go/src/github.com/taehoio/idl
-PROTOBUF_COMMIT_SHA=1ddffd5a6465f0de4e97bb20040022e75127c465
-
-docker run --rm -i --name protobuf -v "$(pwd)":$IDL_PATH -w $IDL_PATH --entrypoint /bin/sh taehoio/protobuf:$PROTOBUF_COMMIT_SHA -c '\
-  buf generate \
-'
